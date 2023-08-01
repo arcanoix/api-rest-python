bcb38a7d80917e45df529ddc14948f855e1b5141
thavoo <gustavoh.2312@gmail.com>
2023-08-01 12:37:01 -0400

    upd

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    objects = None
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
